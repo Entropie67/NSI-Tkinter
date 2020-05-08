cfdd475cf6c5b6c7ece87cd54e7a5552f0019bf8
Elophe <olivier.elophe@gmail.com>
2020-05-08 13:20:53 +0200

    input

--- retouche.py
+++ retouche.py
@@ -9,5 +9,12 @@ bouton = Button(fenetre, text="Quit", command=fenetre.quit)
 bouton.pack()
 
 
+variable = StringVar()
+variable.set("Tapez votre texte")
+entre = Entry(fenetre, textvariable=variable, width=50)
+entre.pack()
+
+
+
 fenetre.mainloop()
 
