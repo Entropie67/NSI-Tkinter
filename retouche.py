133ce100e0a198db7d6f21706f5bde037609164b
Elophe <olivier.elophe@gmail.com>
2020-05-08 13:31:27 +0200

    fonction

--- retouche.py
+++ retouche.py
@@ -1,14 +1,36 @@
 from tkinter import *
+import random
 
+
+
+def choix():
+    jours = [
+        "lundi",
+        "mardi",
+        "mercredi",
+        "jeudi",
+        "vendredi",
+        "samedi",
+        "dimanche"
+    ]
+    print(random.choice(jours))
+    return random.choice(jours)
+
+# On crée la fenêtre principale
 fenetre = Tk()
 
-label = Label(fenetre, text="Coucou !")
+# On crée un petit label
+label = Label(fenetre, text="Choix d'un jour !")
 label.pack()
 
-bouton = Button(fenetre, text="Quit", command=fenetre.quit)
+bouton_choix = Button(fenetre, text="Choix", command=choix)
+bouton_choix.pack()
+
+# on crée un bouton
+bouton = Button(fenetre, text="Quit", command=fenetre.destroy)
 bouton.pack()
 
-
+# Un input
 variable = StringVar()
 variable.set("Tapez votre texte")
 entre = Entry(fenetre, textvariable=variable, width=50)
