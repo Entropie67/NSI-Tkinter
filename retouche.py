9b59032b096291f67cb2e3aad3b60e83065283fa
Elophe <olivier.elophe@gmail.com>
2020-05-15 13:36:08 +0200

    détails

--- retouche.py
+++ retouche.py
@@ -8,7 +8,15 @@ chemin = "image/pomme.jpg"
 originale = open(chemin)
 
 (L, H) = originale.size
-nouvelle_image = new("RGB", (L, H))
+nouvelle_image = originale.copy()
+
+# On crée la fenêtre principale avec un titre et un fond
+fenetre = Tk()
+fenetre.title("PhotoSturm")
+fenetre['bg'] = '#FA8072'
+image_originale = ImageTk.PhotoImage(originale)
+image_nouvelle = ImageTk.PhotoImage(nouvelle_image)
+image2 = ImageTk.PhotoImage(Image.open("image/resultat.jpg"))
 
 
 def negatif():
@@ -30,19 +38,14 @@ def claire():
     image3 = ImageTk.PhotoImage(nouvelle_image)
     canvas2.itemconfig(1, image=image3)
 
-# On crée la fenêtre principale avec un titre et un fond
-fenetre = Tk()
-fenetre.title("PhotoSturm")
-fenetre['bg'] = '#FA8072'
-image_originale = ImageTk.PhotoImage(originale)
-
 
 # On crée un petit label
 # On crée les titres des deux parties principale de l'appli
 titre_depart = Label(fenetre, text="Image de départ", padx=10)
-titre_fin = Label(fenetre, text="Image modifiée", padx=10)
 titre_depart.grid(row=0, column=0, columnspan=6, sticky=N, pady=2, padx=5)
+titre_fin = Label(fenetre, text="Image modifiée", padx=10)
 titre_fin.grid(row=0, column=6, columnspan=6, sticky=N, pady=2, padx=5)
+# Les boutons
 bouton_negatif = Button(fenetre, text="Négatif", command=negatif)
 bouton_negatif.grid(row=2, column=0, sticky=N, pady=2)
 bouton_clair = Button(fenetre, text="Plus clair", command=claire)
@@ -57,18 +60,15 @@ variable.set(chemin)
 entre = Entry(fenetre, textvariable=variable, width=50, bg = '#999999', fg='blue')
 entre.grid(row=2, column=6, sticky=W, pady=2)
 
-
-
-
+# Les images
 canvas = Canvas(fenetre, width=400, height=400)
 canvas.create_image(50, 5, anchor=NW, image=image_originale)
 canvas.grid(row=1, column=0, columnspan=6, sticky=N, pady=2)
 
-image2 = ImageTk.PhotoImage(Image.open("image/resultat.jpg"))
-
 canvas2 = Canvas(fenetre, width=400, height=400)
-i = canvas2.create_image(0, 0, anchor=NW, image=image_originale)
+i = canvas2.create_image(0, 0, anchor=NW, image=image_nouvelle)
 canvas2.grid(row=1, column=6, columnspan=6, sticky=N, pady=2)
+
 # Le bouton qui permet de quitter l'application.
 bouton = Button(fenetre, text="Quit", fg="red", bg="white", relief="raised",
                 overrelief="sunken", command=fenetre.destroy)
